b05c21157bdd004f4f44f6bb06c7a562a164c1f8
anikonchuk <anikonch@gmail.com>
2019-04-07 16:13:39 -0400

    Complete practice 6.php

--- 6.php
+++ 6.php
@@ -5,24 +5,31 @@
 	<section class="content">
 
 		<aside class="col-xs-4">
-		
+
 		<?php Navigation();?>
-			
+
 		</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
- 
 
-	<?php  
 
+	<?php
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	echo $name;
+}
 /*  Step1: Make a form that submits one value to POST super global
 
 
  */
 
-	
+
 ?>
+<form action="6.php" method="post">
+	<input type="text" name="name">
+	<input type="submit" name="submit">
+</form>
 
 
 </article><!--MAIN CONTENT-->
