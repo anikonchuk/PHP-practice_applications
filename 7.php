7e0e4376ee367807b5af54eef8a1a4e3340020a4
anikonchuk <anikonch@gmail.com>
2019-04-09 12:09:04 -0400

    Add solution for practice 7.php

--- 7.php
+++ 7.php
@@ -1,22 +1,22 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
-    
+
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-			
+
+
 		</aside><!--SIDEBAR-->
 
 
 	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
+
+
+
+	<?php
 
 	/*  Step 1 - Create a database in PHPmyadmin
 
@@ -27,7 +27,14 @@
 		Step 4 - Connect to Database and read data
 
 */
-	
+  $connection = mysqli_connect('localhost', 'root', '', 'practice_app');
+  $query = "SELECT * FROM fav_dogs";
+  $result = mysqli_query($connection, $query);
+  while($row = mysqli_fetch_assoc($result)){
+    echo $row['name'] . "- " . $row['breed'] . "<br />";
+  }
+
+
 	?>
 
 
